76bebfc581597d6598b0c94b74f9bba48cc90062
he0o0nje <dudwogjswp@naver.com>
2023-06-07 16:01:14 +0900

    6-7 flex

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { StyleSheet, Image, View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.textcontainer}>
-        <Text style={styles.textstyle}>헌제짱!</Text>
+      <View style={styles.container1}></View>
+      <View style={styles.container2}>
+        <View style={styles.inner1}></View>
+        <View style={styles.inner2}>
+          <View style={styles.content}>
+            <Text>!!메롱!!</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -14,22 +20,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignContent: "center",
   },
-  textcontainer: {
-    margin: 10,
-    padding: 10,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: "#000",
-    borderStyle: "dotted",
+  container1: {
+    flex: 1,
+    backgroundColor: "red",
   },
-  textstyle: {
-    color: "red",
-    fontSize: 20,
-    fontWeight: "700",
-    textAlign: "center",
+  container2: {
+    flex: 2,
+    flexDirection: "row",
+    backgroundColor: "yellow",
+  },
+  inner1: {
+    flex: 1,
+    backgroundColor: "blue",
+  },
+  inner2: {
+    flex: 4,
+    //justifyContent: "flex-start",
+    backgroundColor: "orange",
+    alignItems: "flex-end",
+  },
+  content: {
+    width: 50,
+    height: 50,
+    backgroundColor: "green",
   },
 });
